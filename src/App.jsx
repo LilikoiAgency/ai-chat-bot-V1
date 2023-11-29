78125cb62f163b6b64d04df0ead20a2883be1554
Frank <francisco.hernandez4412@gmail.com>
2023-11-29 11:38:19 -0800

    update to add nlp

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css' 
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css'
 import {MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator} from '@chatscope/chat-ui-kit-react'
+import nlp from 'compromise';
 
 
 
@@ -39,47 +40,31 @@ function App() {
 //
   async function processMessageToChatGpt(chatMessages) {
     try {
-      let apiMessages = chatMessages.map((messageObject) => {
-        let role = "";
-        if (messageObject.sender === "Gregory the great ") {
-          role = "assistant";
-        } else {
-          role = "user";
-        }
-        return { role: role, content: messageObject.message };
+      const apiMessages = chatMessages.map((messageObject) => {
+        const role = messageObject.sender === "Gregory the great " ? "assistant" : "user";
+        return { role, content: messageObject.message };
       });
   
       // Check if the user's message mentions setting an appointment
       const userMessage = chatMessages[chatMessages.length - 1].message.toLowerCase();
-      if (userMessage.includes('schedule appointment') || userMessage.includes('set appointment')) {
-        // Replace this function with your logic to generate the actual appointment link
-          function generateAppointmentLink() {
-            // Example: Generating a static hyperlink to www.sempersolaris.com/appointment
-            return '<a href="https://appointment.sempersolaris.com/" target="_blank" rel="noopener noreferrer">Schedule your appointment</a>';
-          }
+      if (containsAppointmentTrigger(userMessage)) {
         // If the user mentions setting an appointment, generate and send the appointment link
         const appointmentLink = generateAppointmentLink();
         const appointmentResponse = `You can schedule your appointment using this link: ${appointmentLink}`;
   
-        setMessages([...chatMessages, {
-          message: appointmentResponse,
-          sender: "Gregory the great "
-        }]);
+        setMessages([...chatMessages, { message: appointmentResponse, sender: "Gregory the great " }]);
         setTyping(false);
         return;
       }
   
       const systemMessage = {
         role: "system",
-        content: "Your name is Gary, a semper solaris employee who is friendly and nice! you are short and accurate with your responses and you are always looking to help educate people on how solar can help them save money and become energy independent. You want them to book an appointment"
+        content: "Your name is Gary, a semper solaris employee who is friendly and nice! you are short and accurate with your responses and you are always looking to help educate people on how solar can help them save money and become energy independent. You want them to book an appointment, also once a user provides you with there name, email, and phone number please reiterate it back in this format: Name: , Email: , Phone: . but only if you have all the informmation at once. Make sure you do not tell them you will reiterate back just sound natural but tell them to confirm it is correct"
       };
   
       const apiRequestBody = {
-        "model": "gpt-3.5-turbo",
-        "messages": [
-          systemMessage,
-          ...apiMessages
-        ]
+        model: "gpt-3.5-turbo",
+        messages: [systemMessage, ...apiMessages],
       };
   
       const response = await fetch("https://api.openai.com/v1/chat/completions", {
@@ -109,11 +94,28 @@ function App() {
     }
   }
 
+  function containsAppointmentTrigger(userMessage) {
+    // Define keywords or phrases that indicate the intent to schedule an appointment
+    const appointmentTriggers = ["appointment", "set appointment", "book appointment", "consultion"];
+  
+    // Tokenize the user's message
+    const tokens = nlp(userMessage).out('array');
+  
+    // Check if any of the triggers are present in the tokens
+    return appointmentTriggers.some(trigger =>
+      tokens.some(token => token.toLowerCase().includes(trigger))
+    );
+  }
+  function generateAppointmentLink() {
+    // Example: Generating a static hyperlink to www.sempersolaris.com/appointment
+    return '<a href="https://appointment.sempersolaris.com/" target="_blank" rel="noopener noreferrer">Schedule your appointment</a>';
+  }
+
 //
   return (
     <>
       <div className="App">
-      <div style={{position: "relative", height:"800px", width: "400px"}}>
+      <div style={{position: "relative", height:"800px", width: "400px", textAlign: 'left'}}>
         <MainContainer>
           <ChatContainer>
             <MessageList typingIndicator={typing ? <TypingIndicator content="Gregory the great is typing "/> : null }>

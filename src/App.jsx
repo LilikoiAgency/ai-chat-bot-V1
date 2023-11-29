be6478621de5e2100fbd0c41bfa1014148de07df
Frank <francisco.hernandez4412@gmail.com>
2023-11-29 09:56:36 -0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -36,43 +36,62 @@ function App() {
     // process messages
     await processMessageToChatGpt(newMessages);
   }
-
-  async function processMessageToChatGpt(chatMessages){
-    
-    let apiMessages = chatMessages.map((messageObject) => {
-      let role = "";
-      if(messageObject.sender === "Gregory the great ") {
-        role ="assistant"
-      } else{
-        role = "user"
+//
+  async function processMessageToChatGpt(chatMessages) {
+    try {
+      let apiMessages = chatMessages.map((messageObject) => {
+        let role = "";
+        if (messageObject.sender === "Gregory the great ") {
+          role = "assistant";
+        } else {
+          role = "user";
+        }
+        return { role: role, content: messageObject.message };
+      });
+  
+      // Check if the user's message mentions setting an appointment
+      const userMessage = chatMessages[chatMessages.length - 1].message.toLowerCase();
+      if (userMessage.includes('schedule appointment') || userMessage.includes('set appointment')) {
+        // Replace this function with your logic to generate the actual appointment link
+          function generateAppointmentLink() {
+            // Example: Generating a static hyperlink to www.sempersolaris.com/appointment
+            return '<a href="https://appointment.sempersolaris.com/" target="_blank" rel="noopener noreferrer">Schedule your appointment</a>';
+          }
+        // If the user mentions setting an appointment, generate and send the appointment link
+        const appointmentLink = generateAppointmentLink();
+        const appointmentResponse = `You can schedule your appointment using this link: ${appointmentLink}`;
+  
+        setMessages([...chatMessages, {
+          message: appointmentResponse,
+          sender: "Gregory the great "
+        }]);
+        setTyping(false);
+        return;
       }
-      return {role: role, content: messageObject.message}
-    });
-    
-    const systemMessage ={
-      role: "system",
-      content: "Talk to me like a sleazy car salesman! make sure to be very persuasive and argue that the ford pinto is arguably the best car ever. make sure to keep it short and simple."
-    }
-
-    const apiRequestBody = {
-      "model": "gpt-3.5-turbo",
-      "messages": [
-        systemMessage, 
-        ...apiMessages
-      ]
-    }
-
-    await fetch("https://api.openai.com/v1/chat/completions", 
-    {
-      method: "POST",
-      headers: {
-        "Authorization": `Bearer ${import.meta.env.VITE_REACT_APP_APP_KEY_TOKEN}`,
-        "Content-Type": "application/json" 
-      },
-      body: JSON.stringify(apiRequestBody)
-    }).then((data) => {
-      return data.json();
-    }).then((data) => {
+  
+      const systemMessage = {
+        role: "system",
+        content: "Your name is Gary, a semper solaris employee who is friendly and nice! you are short and accurate with your responses and you are always looking to help educate people on how solar can help them save money and become energy independent. You want them to book an appointment"
+      };
+  
+      const apiRequestBody = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+          systemMessage,
+          ...apiMessages
+        ]
+      };
+  
+      const response = await fetch("https://api.openai.com/v1/chat/completions", {
+        method: "POST",
+        headers: {
+          "Authorization": `Bearer ${import.meta.env.VITE_REACT_APP_APP_KEY_TOKEN}`,
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(apiRequestBody)
+      });
+  
+      const data = await response.json();
       console.log(data);
       console.log(data.choices[0].message.content);
       setMessages([...chatMessages, {
@@ -80,17 +99,17 @@ function App() {
         sender: "Gregory the great "
       }]);
       setTyping(false);
-    }).catch((error) => {
+    } catch (error) {
       console.error('Error communicating with the chatbot:', error);
       setMessages([...chatMessages, {
         message: 'Error communicating with the chatbot',
         sender: "System"
       }]);
       setTyping(false);
-    }); 
+    }
   }
 
-
+//
   return (
     <>
       <div className="App">

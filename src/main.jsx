287d1271b3018fcdc4abc5d4882b4288d808bc8e
Frank <francisco.hernandez4412@gmail.com>
2024-03-18 10:39:21 -0700

    path update

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Note the addition of Routes
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
 import App from './App.jsx';
 import './index.css';
 import AssistantBot from './assistantBot.jsx';
@@ -8,12 +8,8 @@ import AssistantBot from './assistantBot.jsx';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      {/* Use the Routes component to wrap Route components */}
       <Routes>
-        {/* Route for /assistant-api */}
         <Route path="/" element={<App />} />
-        
-        {/* Route for other paths */}
         <Route path="/assistant-api" element={<AssistantBot />} />
       </Routes>
     </Router>
